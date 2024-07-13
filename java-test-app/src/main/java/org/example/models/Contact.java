329c1cfd4680d4ff2f65e59ed16ad5a77fdf6a4e
sandraLopez14 <u19221473@utp.edu.pe>
2024-07-12 22:27:58 -0500

    commit de sandra

---
+++ java-test-app/src/main/java/org/example/models/Contact.java
@@ -0,0 +1,50 @@
+package org.example.models;
+
+
+public class Contact {
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public Contact() {
+    }
+
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
